8b59c208dd10aefc6372b1ff9e6b5687587b8f0f
uriassm <uriassmira@gmail.com>
2022-06-24 15:36:49 -0700

    Uploaded files and folders for working Cycle game.

---
+++ cycle/game/scripting/move_actors_action.py
@@ -0,0 +1,29 @@
+from game.scripting.action import Action
+
+
+class MoveActorsAction(Action):
+    """
+    An update action that moves all the actors.
+    
+    The responsibility of MoveActorsAction is to move all the actors that have a velocity greater
+    than zero.
+    """
+
+    def execute(self, cast, script):
+        """Executes the move actors action.
+
+        Args:
+            cast (Cast): The cast of Actors in the game.
+            script (Script): The script of Actions in the game.
+        """
+        actors = cast.get_all_actors()
+        snake = cast.get_first_actor("snakes")
+        snake2 = cast.get_second_actor("snakes")
+        for actor in actors:
+            actor.move_next()
+
+        snake.grow_tail(1)
+        snake2.grow_tail(1)
+      
+
+        
